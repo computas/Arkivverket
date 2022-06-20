f4a23633d8ce5f31d0710c243945d749eeb9f1ff
Helene Jonson <hejox@computas.com>
2022-06-20 09:24:27 +0200

    Add page navigation

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './About.scss';
+import {Button} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 function About() {
-  return (
-    <div className="About">
-      <h1>About 2022</h1>
-    </div>
+    const navigateTo = useNavigate()
+    return (
+        <div className="About">
+            <h1>About 2022</h1>
+            <Button
+                type="button"
+                className="btn btn-secondary btn-sm"
+                onClick={(e) => {
+                    navigateTo("/")
+                }}
+            >
+                To Home
+            </Button>
+        </div>
   );
 }
 
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,36 +1,45 @@
 import React, { useState } from 'react';
 import './Home.scss';
 import { Button } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+    const navigateTo = useNavigate();
+    const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  function startDiv() {
-    setIsVisible(!isVisible)
-    console.log("ee")
-  }
+    function startDiv() {
+        setIsVisible(!isVisible)
+    }
 
     return (
         <div className="home">
-          <h1>Home</h1>
-          <div className='btn-div'>
-            <h2>Testbutton</h2>
+            <h1>Home</h1>
+            <div className='btn-div'>
+                <h2>Test-button</h2>
+                <Button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={(e) => {
+                    startDiv()
+                    }}
+                    >
+                    Click me
+                </Button>
+            </div>
+            <div id='tester' style={{display: isVisible? 'block' : 'none'}}>
+                <p>hello</p>
+            </div>
             <Button
-              type="button"
-              className="btn btn-secondary btn-sm"
-              onClick={(e) => {
-                startDiv()
-              }}
-            >
-              Click me
+                type="button"
+                className="btn btn-secondary btn-sm"
+                onClick={(e) => {
+                navigateTo("/about")
+                }}
+                >
+                To About
             </Button>
-          </div>
-          <div id='tester' style={{display: isVisible? 'block' : 'none'}}>
-              <p>hello</p>
-          </div>
-          
         </div>
-      );
+    );
 }
 
 export default Home;
